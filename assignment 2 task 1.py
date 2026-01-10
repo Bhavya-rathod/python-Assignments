49967a48073dd93b508baabeae2bd3866c5b5aa1
Bhavya-rathod <rathodbnv@gmail.com>
2026-01-10 22:11:13 +0530

    initial commit

---
+++ assignment 2 task 1.py
@@ -0,0 +1,8 @@
+# Take integer input from the user
+number = int(input("Enter an integer: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
